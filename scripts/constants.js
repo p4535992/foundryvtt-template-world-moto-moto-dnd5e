622ce975d5b086542fac405a16b948369cad0428
p4535992 <tentimarco0@gmail.com>
2023-03-29 21:08:47 +0200

    beta

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,7 +1,9 @@
 const CONSTANTS = {
-	MODULE_ID: "template-world-moto-moto-dnd5e-module",
-	PATH: `modules/template-world-moto-moto-dnd5e-module/`,
-  ADDITIONAL_MODULE_PACKS: [`dnd5e`]
+	MODULE_ID: "template-world-moto-moto-dnd5e",
+	PATH: `modules/template-world-moto-moto-dnd5e/`,
+	ADDITIONAL_MODULE_PACKS: [`dnd5e`],
+	FLAG: "twmmoda",
+	PACK_ADVENTURE_ID: "template-world-moto-moto-dnd5e-adventures"
 };
 CONSTANTS.PATH = `modules/${CONSTANTS.MODULE_NAME}/`;
 export default CONSTANTS;
--- scripts/world.js
+++ scripts/world.js
@@ -1,10 +1,12 @@
+import CONSTANTS from "./constants";
+
+const ID = CONSTANTS.MODULE_ID;
 /*
+// Logo
 const logo = document.querySelector("img#logo");
-logo.src = "worlds/CONSTANTS.MODULE_ID/styles/ws.svg";
+logo.src = `modules/${CONSTANTS.MODULE_ID}/styles/ws.svg`;
 logo.addEventListener("click", () =>
-    window.open(game.world.data.authors
-        .filter(a => a.name === "World Smiths")
-        .map(a => a.url)[0])
+	window.open(game.world.data.authors.filter(a => a.name === "World Smiths").map(a => a.url)[0])
 );
 
 const style = document.createElement("style");
@@ -19,3 +21,198 @@ img#logo:hover {
 }`;
 document.head.appendChild(style);
 */
+/*
+const style = document.createElement("style");
+style.textContent = ".journal-entry-page:hover .editor-edit { display: block !important }";
+document.head.append(style);
+*/
+// Custom flag
+Hooks.on("renderJournalSheet", classIfFlag);
+Hooks.on("renderActorSheet", classIfFlag);
+Hooks.on("renderItemSheet", classIfFlag);
+Hooks.on("renderAdventureImporter", classIfFlag);
+
+function classIfFlag(app, [html]) {
+	let doc = app.document;
+	do {
+		if (doc.getFlag("world", CONSTANTS.FLAG)) {
+			html.classList.add(CONSTANTS.FLAG);
+		}
+	} while ((doc = doc.parent));
+}
+
+const getImportOptions = ({ id, title, description }) => ({
+	displayJournal: {
+		label: "Display Introduction Journal",
+		default: true,
+		handler: () => game.journal.getName(title)?.sheet.render(true),
+	},
+	configureWorld: {
+		label: "Configure World details",
+		default: false,
+		handler: () =>
+			fetch(getRoute("/setup"), {
+				headers: {
+					"content-type": "application/json",
+				},
+				body: JSON.stringify({
+					id,
+					title,
+					description,
+					background: `modules/${CONSTANTS.MODULE_ID}/content/${id}/scenes/backgrounds/login.webp`,
+					nextSession: null,
+					action: "editWorld",
+				}),
+				method: "POST",
+			}),
+	},
+});
+
+// Add HTML options to the importer form
+Hooks.on("renderAdventureImporter", (app, html) => {
+	const { adventure } = app;
+	if (adventure.getFlag("world", CONSTANTS.FLAG) !== ID) return;
+
+	const importOptions = getImportOptions({
+		id: adventure.getFlag("world", CONSTANTS.FLAG),
+		title: adventure.name,
+		description: adventure.description,
+	});
+
+	// Format options HTML
+	let options = `<section class="import-form"><h2>Importer Options</h2>`;
+	for (const [name, option] of Object.entries(importOptions)) {
+		options += `<div class="form-group">
+			<label class="checkbox">
+				<input type="checkbox" name="${name}" title="${option.label}" ${option.default ? "checked" : ""}/>
+				${option.label}
+			</label>
+		</div>`;
+	}
+	options += `</section>`;
+
+	// Insert options
+	html.find(".adventure-contents").append(options);
+	app.setPosition();
+});
+
+// Handle options supported by the importer
+Hooks.on("importAdventure", async (adventure, formData) => {
+	const importOptions = getImportOptions({
+		id: adventure.getFlag("world", CONSTANTS.FLAG),
+		title: adventure.name,
+		description: adventure.description,
+	});
+
+	for (const [name, option] of Object.entries(importOptions)) {
+		if (formData[name]) await option.handler(adventure, option);
+	}
+});
+
+// Import Tour
+class ImportTour extends Tour {
+	async _preStep() {
+		await super._preStep();
+
+		if (this.currentStep.sidebarTab) {
+			ui.sidebar.activateTab(this.currentStep.sidebarTab);
+		}
+
+		if (this.currentStep.id === "adventurePack") {
+			await new Promise(resolve => {
+				game.packs.get(`${ID}.${CONSTANTS.PACK_ADVENTURE_ID}`).render(true);
+				Hooks.on("renderCompendium", app => {
+					if (app.metadata.packageName === ID) {
+						resolve();
+					}
+				});
+			});
+		} else if (this.currentStep.id === "importForm") {
+			await new Promise(resolve => {
+				Hooks.on("renderAdventureImporter", app => {
+					if (app.adventure.getFlag("world", CONSTANTS.FLAG)) {
+						resolve();
+					}
+				});
+			});
+		}
+	}
+}
+
+Hooks.on("setup", () => {
+	const description =
+		"This Tour will guide you through importing World Smiths's contents.";
+	let title = "Importing an adventure";
+	if (ID !== CONSTANTS.FLAG) {
+		const t = game.modules.get(ID)?.title;
+		if (t) {
+			title += `: ${t}`;
+		}
+	}
+
+	game.settings.register(ID, "importTour", {
+		scope: "world",
+		config: false,
+		type: Boolean,
+		default: true,
+	});
+
+	game.tours.register(
+		ID,
+		"import",
+		new ImportTour({
+			title,
+			description,
+			display: true,
+			restricted: true,
+			steps: [
+				{
+					id: "welcome",
+					selector: "",
+					title: "Welcome to your adventure",
+					content: description,
+				},
+				{
+					id: "compendiumPacks",
+					selector: ".tabs>a[data-tab='compendium']",
+					title: "Compendium Packs",
+					content: "Open the Compendium sidebar tab to view your Adventure compendiums.",
+					sidebarTab: "compendium",
+				},
+				{
+					id: "adventurePack",
+					selector: `[data-pack='${ID}.${CONSTANTS.PACK_ADVENTURE_ID}']`,
+					title: "Adventure Pack",
+					content: "Open the adventure compendium pack to view it's contents.",
+				},
+				{
+					id: "adventureDoc",
+					selector: `.compendium[data-pack='${ID}.${CONSTANTS.PACK_ADVENTURE_ID}'] .directory-list`,
+					title: "Choose an Adventure",
+					content: "Pick an adventure to import into your world.",
+					tooltipDirection: TooltipManager.TOOLTIP_DIRECTIONS.UP,
+				},
+				{
+					id: "importForm",
+					selector: `.adventure-importer.${CONSTANTS.PACK_ADVENTURE_ID} .import-form`,
+					title: "Import Options",
+					content: "Configure the various options for this import.",
+					tooltipDirection: TooltipManager.TOOLTIP_DIRECTIONS.UP,
+				},
+				{
+					id: "import",
+					selector: `.adventure-importer.${CONSTANTS.PACK_ADVENTURE_ID} button[type='submit']`,
+					title: "Import the Adventure!",
+					content: "Just click this button to import the adventure. Happy gaming! 🎉",
+				}
+			]
+		})
+	);
+});
+
+Hooks.on("ready", () => {
+	if (game.settings.get(ID, "importTour")) {
+		game.tours.get(`${ID}.import`).start();
+		game.settings.set(ID, "importTour", false);
+	}
+});
